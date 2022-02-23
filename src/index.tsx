46cddb84ea8e9b763bb0db10f33c584e9fc61bf8
Dominik Kuenkele <dominik.kuenkele@outlook.com>
2022-02-23 15:16:44 +0100

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from "react-dom";
+import "./styles.scss";
+
+
+
+const rootElement = document.getElementById("root");
+
+function App() {
+    return <div>Hello World!</div>;
+}
+
+ReactDOM.render(
+    <App />,
+    rootElement);
